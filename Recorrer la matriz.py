28896d8d440710c0a02e7aa9f316433f591a506c
Tamayo-Goya <alejandro7tamayo@gmail.com>
2025-10-29 20:53:39 +0100

    Ejercicio 3
    Removed spaces before comments for consistency.

--- Recorrer la matriz.py
+++ Recorrer la matriz.py
@@ -1,41 +1,41 @@
 matriz = []  
-# Definimos la lista donde almacenará todo
+#Definimos la lista donde almacenará todo
 
 contador = 1
-# Definimos un contador solo para mostrar el número
-# del elemento que estamos añadiendo
+#Definimos un contador solo para mostrar el número
+#del elemento que estamos añadiendo
 
 for i in range(3):  
-    # Ya que la matriz es 3x3, solo vamos a recorrer 3 veces
+    #Ya que la matriz es 3x3, solo vamos a recorrer 3 veces
     
     fila = []  
-    # Definimos fila para almacenar los datos
+    #Definimos fila para almacenar los datos
 
     for j in range(3):  
         valor = int(input(f"Elemento {contador}: "))
-        # Pedimos al usuario los valores para la matriz
+        #Pedimos al usuario los valores para la matriz
 
         fila.append(valor) 
-        # Añadimos el número a la fila
+        #Añadimos el número a la fila
 
         contador = contador + 1
         
     matriz.append(fila)  
-    # Añadimos toda la fila a la matriz
+    #Añadimos toda la fila a la matriz
 
 
 print("\nMatriz completa:")
 for fila in matriz:
     print(fila)
-# Mostramos la matriz   
+#Mostramos la matriz   
 
 
 print("\nElementos de la matriz uno por uno:")
 for n in range(len(matriz)):         
-    # Recorre las filas
+    #Recorre las filas
 
     for p in range(len(matriz[n])):  
-        # Recorre las columnas
+        #Recorre las columnas
 
         print(f"Elemento en [{n}][{p}] = {matriz[n][p]}")
 #Recorremos toda la matriz e imprimimos cada elemento
